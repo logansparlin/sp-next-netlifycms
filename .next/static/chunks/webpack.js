c79204bfe6801e58b5935fb69e4b5e5b490d36b8
Logan Sparlin <logan.sparlin@gmail.com>
2021-03-17 12:09:22 -0500

    new .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "eee51b44b4c5608c8777";
+/******/ 	var hotCurrentHash = "8ecf2b6ddc6d4d30e69b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
